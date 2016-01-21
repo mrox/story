62d4da54193d07a2974b71cc4508a34760d7d3c7
Tuyen Tran <tuyen.appdev@gmail.com>
2016-01-21 09:13:44 +0700

    Swift

---
+++ storySW/HomeCell.swift
@@ -0,0 +1,39 @@
+//
+//  HomeCell.swift
+//  storySW
+//
+//  Created by Tran Trung Tuyen on 20/01/2016.
+//  Copyright © 2016 Tran Trung Tuyen. All rights reserved.
+//
+
+import UIKit
+
+class HomeCell: UITableViewCell {
+
+    @IBOutlet weak var titleStoryLabel: UILabel!
+    @IBOutlet weak var detailStoryLabel: UILabel!
+    @IBOutlet weak var coverImageView: UIImageView!
+    
+    @IBOutlet weak var bottomCellView: UIView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        self.coverImageView.clipsToBounds = true
+        self.coverImageView.layer.cornerRadius = CGRectGetHeight(self.coverImageView.frame)/2
+        
+        self.bottomCellView.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1).CGColor
+        self.bottomCellView.layer.shadowOffset = CGSizeMake(0.0, 2.0)
+        self.bottomCellView.layer.shadowOpacity = 0.5
+        self.bottomCellView.layer.shadowRadius = 0.0
+        self.bottomCellView.layer.masksToBounds = false
+//        self.bottomCellView.layer.cornerRadius = 4.0
+        
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ storySW/HomeViewController.swift
@@ -0,0 +1,112 @@
+//
+//  HomeViewController.swift
+//  storySW
+//
+//  Created by Tran Trung Tuyen on 20/01/2016.
+//  Copyright © 2016 Tran Trung Tuyen. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+import Kingfisher
+
+class HomeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var arrRes = [[String:AnyObject]]() //Array of dictionary
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setNavStyle()
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.estimatedRowHeight = 350
+        tableView.rowHeight = UITableViewAutomaticDimension;
+        
+        self.getTableData();
+    }
+    
+    func setNavStyle() {
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 0.31, green: 0.42, blue: 0.64, alpha: 1)
+        self.navigationController?.navigationBar.translucent = false;
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func getTableData() {
+        let apiurl = "http://ebook2.local.192.168.1.15.xip.io/api/story"
+//        let apiurl = "http://ebook2.local/api/story"
+        Alamofire.request(.GET, apiurl)
+            .responseJSON { responseData in
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                
+                if let resData = swiftyJsonVar["data"].arrayObject {
+                    self.arrRes = resData as! [[String:AnyObject]]
+                }
+                if self.arrRes.count > 0 {
+                    self.tableView.reloadData()
+//                    print(self.arrRes);
+                }
+        }
+    }
+    
+    //Tableview Datasource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.arrRes.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:HomeCell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! HomeCell
+        let rowData = self.arrRes[indexPath.row]
+        
+        
+        
+        cell.titleStoryLabel.text = rowData["name"] as? String
+        
+        let detailText = rowData["description"] as? String
+        let indexString = ((detailText?.characters.indexOf(".")) != nil) ? detailText?.characters.indexOf("."):detailText?.startIndex.advancedBy(200)
+        
+        cell.detailStoryLabel.text = (detailText?.substringToIndex(indexString!))!+("...")
+        
+        cell.coverImageView.kf_setImageWithURL(NSURL(string: (rowData["imgurl"] as? String)!)!);
+        
+        
+        //Justified UIlabel
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = NSTextAlignment.Justified
+        
+        let attributedString = NSAttributedString(string: cell.detailStoryLabel.text!,
+            attributes: [
+                NSParagraphStyleAttributeName: paragraphStyle,
+                NSBaselineOffsetAttributeName: NSNumber(float: 0)
+            ])
+        cell.detailStoryLabel.attributedText = attributedString;
+        
+        return cell
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated);
+        tableView.reloadData();
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
